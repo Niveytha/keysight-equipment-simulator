aaab833535ee9c959f7cf9516d4256ce74151624
Niveytha Waran <niveytha@gmail.com>
2022-07-08 11:53:11 +0800

    This week's work completed & working!

--- src/assets/scripts/outputData.py
+++ src/assets/scripts/outputData.py
@@ -10,30 +10,30 @@ import time
 # f = open("src/assets/scripts/i3070_Logs/P1479299-00-D_SPGT19088002596-190503103132-MY58120165-Pass.txt", 'r')
 # g = open("src/assets/scripts/i3070_Logs/output_file.txt", 'w')
 
-# changedVars = {"uutType": sys.argv[1],
-#                "uutTypeRev": sys.argv[2],
-#                "fixtureID": sys.argv[3],
-#                "controller": sys.argv[4],
-#                "boardID": sys.argv[5],
-#                "startDateTime": sys.argv[6],
-#                "startDateTimeChanged": sys.argv[7],
-#                "duration": sys.argv[8],
-#                "durationChanged": sys.argv[9],
-#                "endDateTime": sys.argv[10]
-#                }
+changedVars = {"uutType": sys.argv[1],
+               "uutTypeRev": sys.argv[2],
+               "fixtureID": sys.argv[3],
+               "controller": sys.argv[4],
+               "boardID": sys.argv[5],
+               "startDateTime": sys.argv[6],
+               "startDateTimeChanged": sys.argv[7],
+               "duration": sys.argv[8],
+               "durationChanged": sys.argv[9],
+               "endDateTime": sys.argv[10]
+               }
 
 # !testing - to be deleted
-changedVars = {"uutType": "1479299-00-D",
-               "uutTypeRev": "T1",
-               "fixtureID": "593",
-               "controller": "MY58120165",
-               "boardID": "P1479299-00-D_SPGT19088002596",
-               "startDateTime": "190503103136",
-               "startDateTimeChanged": "true",
-               "duration": "000013",
-               "durationChanged": "false",
-               "endDateTime": "190503103149"
-               }
+# changedVars = {"uutType": "KEYS_1479299-00-D",
+#                "uutTypeRev": "T2",
+#                "fixtureID": "693",
+#                "controller": "KEYS58120165",
+#                "boardID": "KEYS_SPGT19088002596",
+#                "startDateTime": "190503103136",
+#                "startDateTimeChanged": "true",
+#                "duration": "000002",
+#                "durationChanged": "true",
+#                "endDateTime": "190503103149"
+#                }
 
 durationVariations = [-2, -1, 0, 1, 2]
 
@@ -76,9 +76,9 @@ for filename in files:
     # 2. Duration
     if changedVars["durationChanged"] == "true":
         durationVariation = choice(durationVariations)
-        changedVars["duration"] = changedVars["duration"] + durationVariation
-        duration = timedelta(seconds=int(
-            changedVars["duration"]))
+        changedVars["duration"] = int(
+            changedVars["duration"]) + durationVariation
+        duration = timedelta(seconds=changedVars["duration"])
         start = datetime.strptime(
             changedVars["startDateTime"], "%y%m%d%H%M%S")
         changedVars["endDateTime"] = (
@@ -102,6 +102,7 @@ for filename in files:
     lines[0] = '|'.join(batchLine)
     lines[1] = '|'.join(btestLine)
 
+    print("Waiting for: " + changedVars["duration"])
     time.sleep(int(changedVars["duration"]))
     for line in lines:
         g.write(line)
